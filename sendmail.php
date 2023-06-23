951e1b5aacf588b32f922e02320e69458cd2145f
Oleg Voitko <pel6men6.oleg@gmail.com>
2023-06-23 16:01:16 +0300

    feat: add all project

---
+++ sendmail.php
@@ -0,0 +1,23 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	if (!$mail->send()){
+		$message = 'Error'
+	}else {
+		$message = 'Ok'
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
